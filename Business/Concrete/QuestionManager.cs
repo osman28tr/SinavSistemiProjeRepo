278c3559ec79013856cf54c3d587e523bd0290ab
osman28tr <osmantonbul2001@gmail.com>
2022-03-19 19:41:14 +0300

    durumu false olan sorularin karisik gelmesi ogrenci id sine gore ve sonrasinda cozebildigi sorular icin gerekli operasyon eklendi

--- Business/Concrete/QuestionManager.cs
+++ Business/Concrete/QuestionManager.cs
@@ -38,9 +38,9 @@ namespace Business.Concrete
             return _questionDal.GetAll();
         }
 
-        public List<Question> GetQuestionsByNotAnswered()
+        public List<Question> GetQuestionsByNotAnswered(List<QuestionDetail> questionDetails)//çözme durumu false olanlar üzerinden ve student id ye göre rastgele sorular üretildi.
         {
-            int[] dizi2 = GenerateRandomNumber();
+            int[] dizi2 = GenerateRandomNumber(questionDetails); //question id leri rastgele verildi.
             //int questionmenstruation = _questionDal.GetAll().Count;
             //int[] dizi = new int[questionmenstruation];
             List<Question> questions = new List<Question>();
@@ -53,30 +53,74 @@ namespace Business.Concrete
                 //    continue;
                 //}                    
                 //dizi[i] = a;
+
                 int a = dizi2[i];
+                //if (questionDetails.Find(x => x.QuestionId == a) != null) //question'ları false olanları getirsin. ve benim ürettiğim rastgele bir id nin durumu false ise getirsin.
+                //{
+                //    questions.Add(_questionDal.Get(x => x.QuestionId == a));
+                //}
+                //else
+                //{
+
+                //}
                 questions.Add(_questionDal.Get(x => x.QuestionId == a));
             }
             return questions;
         }
-        private int[] GenerateRandomNumber()
+        private int[] GenerateRandomNumber(List<QuestionDetail> questionDetails) //çözme durumu false olanlar geliyor.
         {
-            int questionmenstruation = _questionDal.GetAll().Count;
-            int[] dizi = new int[10];
+            bool durum = true;
+            //int questionmenstruationmax = questionDetails.Max(x => x.QuestionId);
+            //int questionmenstruationmin = questionDetails.Min(x => x.QuestionId);
+            int count = questionDetails.Count; //bize gelen doğru çözme durumu false olan soruların sayısını aldık
+            int[] dizi = new int[10]; //sayıların aynı olmaması için.
+            int[] dizi2 = new int[10]; //bizim durumu false olanların questionid'lerini ilgili diziye atmak için
             for (int i = 0; i < 10; i++)
             {
-                int a = random.Next(1, questionmenstruation + 1);
-                if (dizi.Contains(a))
+                int a = random.Next(0, count); //1 ile gelen verilerin sayısı arasında rastgele sayı ürettik.(bunu indis için yaptım.)
+                for (int j = 0; j < i; j++) //rastgele üretilen sayı dizinin şuanki indisine kadar kontrol ediyor.(bunu 0 için yaptım. çünkü sıfırda patlıyordu.)
+                {
+                    if (dizi[j]==a) //aynı sayı üretmemesi için kontrol ediyor.(yada aynı indis vermemesi için questiondetail için.)
+                    {                        
+                        //j--;
+                        durum = false;
+                        break;
+                        //continue;
+                    }
+                    else
+                    {
+                        durum = true;
+                    }
+                }
+                if (durum == true) //eğer üretilen rastgele sayı (veya indis) dizinin içinde yoksa diziye at ve o indisin question id sini dizi2 ye ver.
+                {
+                    dizi[i] = a;
+                    dizi2[i] = questionDetails[a].QuestionId;
+                }
+                else //eğer random üretilen sayı aynı sayı geldiyse bidaha üretmesi için i azaltıldı.
                 {
                     i--;
                     continue;
                 }
-                dizi[i] = a;
             }
-            return dizi;
+            return dizi2;
         }
         public void Update(Question question)
         {
             _questionDal.Update(question);
+        }
+
+        public List<Question> GetQuestionsByAnswered(List<QuestionDetail> questionDetails)
+        {
+            int valuecount = questionDetails.Count;
+            var questiondetail = questionDetails.Select(x => x.QuestionId).ToList();
+            List<Question> questions = new List<Question>();
+            for (int i = 0; i < valuecount; i++)
+            {
+                int a = questiondetail[i];
+                questions.Add(_questionDal.Get(x => x.QuestionId == a));
+            }
+            return questions;
         }
     }
 }
