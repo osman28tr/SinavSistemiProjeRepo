c875eb8c4a6e9eafe9a837c6b3199e356f80c430
osman28tr <osmantonbul2001@gmail.com>
2022-03-31 17:09:45 +0300

    filreleme,validasyon ve clean code işlemleri(QuestionService)

--- Business/Abstract/IQuestionService.cs
+++ Business/Abstract/IQuestionService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using FluentValidation.Results;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,11 +12,19 @@ namespace Business.Abstract
     public interface IQuestionService
     {
         List<Question> GetAll();
-        Question Get();
+        int GetLastQuestionId();
+        Question Get(int id);
         List<string> Add(Question question);
+        IList<ValidationFailure> Dogrula(Question question);
         void Update(Question question);
         void Delete(Question question);
         List<Question> GetQuestionsByNotAnswered(List<QuestionDetail> questionDetails);
         List<Question> GetQuestionsByAnswered(List<QuestionDetail> questionDetails);
+        List<Question> GetConfirmByQuestions();
+        List<Question> GetNotConfirmByQuestions();
+        List<Question> GetQuestionsByQuestionName(string questionName, int questionConfirmState);
+        List<Question> GetQuestionsByState(int state);
+        List<Question> GetSortByQuestionNameAscending();
+        List<Question> GetSortByQuestionNameDescending();
     }
 }
