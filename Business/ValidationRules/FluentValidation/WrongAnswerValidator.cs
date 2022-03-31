3f6eed007ac28ff6b697cc14b73771434be73b4d
osman28tr <osmantonbul2001@gmail.com>
2022-03-31 17:19:21 +0300

    Soru hazırlamada WrongAnswer icin validasyon

---
+++ Business/ValidationRules/FluentValidation/WrongAnswerValidator.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class WrongAnswerValidator:AbstractValidator<WrongAnswer>
+    {
+        public WrongAnswerValidator()
+        {
+            RuleFor(x => x.WrongAnswerName).NotEmpty().WithMessage("Lütfen yanlış cevap olan seçeneklerinizi boş geçmeyiniz");
+        }
+    }
+}
