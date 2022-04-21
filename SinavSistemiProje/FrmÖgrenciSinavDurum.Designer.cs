3ca1c9ac045c47bae4a9d0774bb7ed380d5df37c
osman28tr <osmantonbul2001@gmail.com>
2022-04-21 18:24:13 +0300

    Ogrencinin konularla ilgili basari durumunu görmesi ve cikti alabilmesinin saglanmasi(UI Katmani)

--- SinavSistemiProje/FrmÖgrenciSinavDurum.Designer.cs
+++ SinavSistemiProje/FrmÖgrenciSinavDurum.Designer.cs
@@ -29,15 +29,18 @@ namespace SinavSistemiProje
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea6 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend6 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series6 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.profilimToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.sınavDurumumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.BtnSinavaGir = new System.Windows.Forms.Button();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.btnEksikKapa = new System.Windows.Forms.Button();
+            this.lblStudent = new System.Windows.Forms.Label();
+            this.btnCiktiAl = new System.Windows.Forms.Button();
+            this.rctBasariDurumu = new System.Windows.Forms.RichTextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
@@ -50,7 +53,7 @@ namespace SinavSistemiProje
             this.sınavDurumumToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(869, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1448, 28);
             this.menuStrip1.TabIndex = 18;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -71,9 +74,9 @@ namespace SinavSistemiProje
             // 
             this.BtnSinavaGir.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.BtnSinavaGir.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.BtnSinavaGir.Location = new System.Drawing.Point(718, 176);
+            this.BtnSinavaGir.Location = new System.Drawing.Point(196, 158);
             this.BtnSinavaGir.Name = "BtnSinavaGir";
-            this.BtnSinavaGir.Size = new System.Drawing.Size(139, 62);
+            this.BtnSinavaGir.Size = new System.Drawing.Size(161, 62);
             this.BtnSinavaGir.TabIndex = 19;
             this.BtnSinavaGir.Text = "Sınava Gir →";
             this.BtnSinavaGir.UseVisualStyleBackColor = true;
@@ -81,17 +84,17 @@ namespace SinavSistemiProje
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            legend2.Name = "Legend1";
-            this.chart1.Legends.Add(legend2);
-            this.chart1.Location = new System.Drawing.Point(12, 99);
+            chartArea6.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea6);
+            legend6.Name = "Legend1";
+            this.chart1.Legends.Add(legend6);
+            this.chart1.Location = new System.Drawing.Point(12, 226);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
-            series2.Legend = "Legend1";
-            series2.Name = "SinavDurumu";
-            this.chart1.Series.Add(series2);
+            series6.ChartArea = "ChartArea1";
+            series6.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series6.Legend = "Legend1";
+            series6.Name = "SinavDurumu";
+            this.chart1.Series.Add(series6);
             this.chart1.Size = new System.Drawing.Size(592, 368);
             this.chart1.TabIndex = 20;
             this.chart1.Text = "chart1";
@@ -100,7 +103,7 @@ namespace SinavSistemiProje
             // 
             this.btnEksikKapa.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.btnEksikKapa.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.btnEksikKapa.Location = new System.Drawing.Point(12, 31);
+            this.btnEksikKapa.Location = new System.Drawing.Point(12, 158);
             this.btnEksikKapa.Name = "btnEksikKapa";
             this.btnEksikKapa.Size = new System.Drawing.Size(178, 62);
             this.btnEksikKapa.TabIndex = 21;
@@ -108,18 +111,51 @@ namespace SinavSistemiProje
             this.btnEksikKapa.UseVisualStyleBackColor = true;
             this.btnEksikKapa.Click += new System.EventHandler(this.btnEksikKapa_Click);
             // 
+            // lblStudent
+            // 
+            this.lblStudent.AutoSize = true;
+            this.lblStudent.Font = new System.Drawing.Font("Segoe Print", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblStudent.ForeColor = System.Drawing.Color.White;
+            this.lblStudent.Location = new System.Drawing.Point(658, 28);
+            this.lblStudent.Name = "lblStudent";
+            this.lblStudent.Size = new System.Drawing.Size(0, 68);
+            this.lblStudent.TabIndex = 22;
+            // 
+            // btnCiktiAl
+            // 
+            this.btnCiktiAl.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCiktiAl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnCiktiAl.Location = new System.Drawing.Point(920, 604);
+            this.btnCiktiAl.Name = "btnCiktiAl";
+            this.btnCiktiAl.Size = new System.Drawing.Size(161, 62);
+            this.btnCiktiAl.TabIndex = 24;
+            this.btnCiktiAl.Text = "Yazdir";
+            this.btnCiktiAl.UseVisualStyleBackColor = true;
+            this.btnCiktiAl.Click += new System.EventHandler(this.btnCiktiAl_Click);
+            // 
+            // rctBasariDurumu
+            // 
+            this.rctBasariDurumu.Location = new System.Drawing.Point(622, 158);
+            this.rctBasariDurumu.Name = "rctBasariDurumu";
+            this.rctBasariDurumu.Size = new System.Drawing.Size(803, 440);
+            this.rctBasariDurumu.TabIndex = 25;
+            this.rctBasariDurumu.Text = "";
+            // 
             // FrmÖgrenciSinavDurum
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Olive;
-            this.ClientSize = new System.Drawing.Size(869, 479);
+            this.ClientSize = new System.Drawing.Size(1448, 671);
+            this.Controls.Add(this.rctBasariDurumu);
+            this.Controls.Add(this.btnCiktiAl);
+            this.Controls.Add(this.lblStudent);
             this.Controls.Add(this.btnEksikKapa);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.BtnSinavaGir);
             this.Controls.Add(this.menuStrip1);
             this.Name = "FrmÖgrenciSinavDurum";
-            this.Text = "FrmÖgrenciSinavDurum";
+            this.Text = "Başarı Durumum";
             this.Load += new System.EventHandler(this.FrmÖgrenciSinavDurum_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -137,5 +173,8 @@ namespace SinavSistemiProje
         private System.Windows.Forms.Button BtnSinavaGir;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.Button btnEksikKapa;
+        private System.Windows.Forms.Label lblStudent;
+        private System.Windows.Forms.Button btnCiktiAl;
+        private System.Windows.Forms.RichTextBox rctBasariDurumu;
     }
 }
--- SinavSistemiProje/FrmÖgrenciSinavDurum.cs
+++ SinavSistemiProje/FrmÖgrenciSinavDurum.cs
@@ -1,10 +1,12 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,10 @@ namespace SinavSistemiProje
         }
         public static int ogrid = 0;
         QuestionDetailManager questionDetailManager = new QuestionDetailManager(new EfQuestionDetailDal());
+        QuestionManager questionManager = new QuestionManager(new EfQuestionDal());
+        StudentManager studentManager = new StudentManager(new EfStudentDal());
+        SubjectManager subjectManager = new SubjectManager(new EfSubjectDal());
+        PrintDialog Prd = new PrintDialog();
         private void BtnSinavaGir_Click(object sender, EventArgs e)
         {
             DialogResult result1 = MessageBox.Show("Hazır mısınız?", " ", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
@@ -44,8 +50,35 @@ namespace SinavSistemiProje
             int falsecount = questionDetailManager.FalseAnswerCount(ogrid);
             chart1.Series["SinavDurumu"].Points.AddXY("Dogru Cevap " + truecount, truecount);
             chart1.Series["SinavDurumu"].Points.AddXY("Yanlis Cevap " + falsecount, falsecount);
-        }
+            lblStudent.Text = "Merhaba " + studentManager.Get(ogrid).StudentName;
 
+            GetSuccessStatus();
+        }
+        private void GetSuccessStatus()
+        {
+            int count = subjectManager.SubjectCount();
+            int countSuccess = 0, countNotSuccess = 0, countEmpty = 0;
+            for (int i = 1; i <= count; i++)
+            {
+                int questionBySubjectCount = questionManager.QuestionBySubject(i);
+                string subjectName = subjectManager.GetSubjectById(i).SubjectName;
+                List<Question> questions = questionManager.GetAll().Where(x => x.SubjectId == i).ToList();
+                for (int j = 0; j < questionBySubjectCount; j++)
+                {
+                    int questionId = questions[j].QuestionId;
+                    countSuccess = countSuccess + questionDetailManager.StudentSuccessQuestionBySubject(questionId, ogrid);
+                    countNotSuccess = countNotSuccess + questionDetailManager.StudentNotSuccessQuestionBySubject(questionId, ogrid);
+                    countEmpty = countEmpty + questionDetailManager.StudentEmptyQuestionBySubject(questionId, ogrid);
+                }
+                rctBasariDurumu.Text = rctBasariDurumu.Text + subjectName + " Konulu sorulardan " + " Size sorulan, " + questionBySubjectCount + " adet sorudan "
+                + countSuccess +
+                " soruyu doğru " + countNotSuccess + " soruyu yanlis " +
+                "bildiniz" + countEmpty + " soruyu ise henüz çözmediniz!!!\n";
+                countSuccess = 0;
+                countNotSuccess = 0;
+                countEmpty = 0;
+            }
+        }
         private void btnEksikKapa_Click(object sender, EventArgs e)
         {
             DialogResult result1 = MessageBox.Show("Hazır mısınız?", " ", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
@@ -55,7 +88,27 @@ namespace SinavSistemiProje
                 frmOgrenciSinavModul2.id = ogrid;
                 frmOgrenciSinavModul2.Show();
                 this.Hide();
-            }        
+            }
+        }
+
+        private void btnCiktiAl_Click(object sender, EventArgs e)
+        {
+            PrintDocument Paper = new PrintDocument();
+            DialogResult writingProcess;
+            writingProcess = Prd.ShowDialog();
+            Paper.PrintPage += Paper_PrintPage;
+            if (writingProcess == DialogResult.OK)
+            {
+                Paper.Print();
+            }
+        }
+
+        private void Paper_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            string write = rctBasariDurumu.Text;
+            Font writeFamily = new Font("Arial", 12);
+            SolidBrush pencil = new SolidBrush(Color.Black);
+            e.Graphics.DrawString(write, writeFamily, pencil, 10, 20);
         }
     }
 }
